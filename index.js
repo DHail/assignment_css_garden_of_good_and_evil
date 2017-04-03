0d809b773d5dfc4b883833ca5c6c031b1cbee1c1
DHail <dhail@ymail.com>
2017-04-03 13:36:34 -0700

    randomly moving text around

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 
 // Set up form body parsing
 const bodyParser = require("body-parser");
@@ -12,9 +13,18 @@ app.use(cookieParser());
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-exphbs.registerPartial('goodList', )
 
 app.use(express.static(__dirname + "/public"));
+
+function randomInsanity(insanity) {
+	let random = {
+		random1: Math.floor(Math.random() * (50 * (insanity -1))),
+		random2: Math.floor(Math.random() * (50 * (insanity -1))),
+		random3: Math.floor(Math.random() * (50 * (insanity -1)))
+	}
+
+	return random;
+}
 
 app.get('/', (req, res) => {
 	let {attitude, food, color, insanity} = req.cookies;
@@ -39,14 +49,16 @@ app.get('/', (req, res) => {
 		purple = true;
 		break;
 	}
-	res.render("index", {attitude, goodAttitude, food, color, blue, red, green, yellow, purple, insanity});
+
+	let randomIn = randomInsanity(insanity);
+
+	res.render("index", {attitude, goodAttitude, food, color, blue, red, green, yellow, purple, insanity, randomIn});
 	
 
 })
 
 app.post("/settings", (req, res) => {
   // res.cookie("name", req.body.name);
-  console.log(req.body);
   res.cookie('attitude', req.body.attitude);
   res.cookie('food', req.body.food);
   res.cookie('color', req.body.color);
